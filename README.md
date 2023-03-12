068ae2d4e3db4dc46ae4b06dee45a1d50b9175a6
xingkun.zhang <1475795322@qq.com>
2023-03-12 22:13:06 +0800

    chore: remove useless files

---
+++ README.md
@@ -0,0 +1,4 @@
+# This is a android project
+
+# opencv
+https://opencv.org/releases/
--- libraries/opencv/build/intermediates/cxx/Debug/5t1y2w55/logs/x86_64/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\xingkun.zhang\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\Chrome\\OpenCV-android-sdk\\sdk\\.cxx\\Debug\\5t1y2w55\\x86_64" ^
-  opencv_jni_shared
